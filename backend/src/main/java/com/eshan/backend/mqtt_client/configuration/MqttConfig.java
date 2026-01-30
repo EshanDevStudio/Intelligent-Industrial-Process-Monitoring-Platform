d3875aa32ee7c671b9dbcda06af48751c0c2692c
EshanDevStudio <eshan.devstudio@gmail.com>
2026-01-30 19:58:38 +0530

    feat(MQTT): Add MQTT brocker connection and topic subscription

--- backend/src/main/java/com/eshan/backend/mqtt_client/configuration/MqttConfig.java
+++ backend/src/main/java/com/eshan/backend/mqtt_client/configuration/MqttConfig.java
@@ -1,4 +1,27 @@
-package com.eshan.backend.mqtt_client.service.configuration;
+package com.eshan.backend.mqtt_client.configuration;
 
+import com.hivemq.client.mqtt.MqttClient;
+import com.hivemq.client.mqtt.mqtt5.Mqtt5BlockingClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class MqttConfig {
+
+    @Value("${mqtt.broker.host}")
+    private String host;
+
+    @Value("${mqtt.broker.port:8883}")
+    private int port;
+
+    @Bean
+    public Mqtt5BlockingClient mqtt5Client() {
+        return MqttClient.builder()
+                .useMqttVersion5()
+                .serverHost(host)
+                .serverPort(port)
+                .sslWithDefaultConfig()
+                .buildBlocking();
+    }
 }
--- backend/src/main/java/com/eshan/backend/mqtt_client/service/MqttService.java
+++ backend/src/main/java/com/eshan/backend/mqtt_client/service/MqttService.java
@@ -1,4 +1,46 @@
 package com.eshan.backend.mqtt_client.service;
 
+import com.hivemq.client.mqtt.mqtt5.Mqtt5BlockingClient;
+import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.nio.charset.StandardCharsets;
+
+@Service
 public class MqttService {
+
+    private static Logger logger = LoggerFactory.getLogger(MqttService.class);
+
+    @Autowired
+    private Mqtt5BlockingClient mqtt5Client;
+
+    @Value("${mqtt.broker.username}")
+    private String username;
+
+    @Value("${mqtt.broker.password}")
+    private String password;
+
+    @PostConstruct
+    public void connectAndSubscribe() {
+        try {
+            // connect to HiveMQ Cloud
+            mqtt5Client.connectWith()
+                    .simpleAuth()
+                    .username(username)
+                    .password(StandardCharsets.UTF_8.encode(password))
+                    .applySimpleAuth()
+                    .send();
+
+            logger.info("MQTT Client connected successfully to broker");
+
+        } catch (Exception e) {
+            logger.error("Failed to connect or subscribe to MQTT broker", e);
+            throw new RuntimeException("MQTT connection failed", e);
+        }
+    }
+
 }
